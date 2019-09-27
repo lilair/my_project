d33485b5afc2f5d2dd951d064ffc2e434b366c20
李成树 <1130270216@qq.com>
2019-09-27 16:16:40 +0800

    Initial commit

---
+++ src/com/changhong/controller/HelloController.java
@@ -0,0 +1,11 @@
+package com.changhong.controller;
+
+/**
+ * @author lilair
+ * @create 2019/09/27 15:25
+ */
+public class HelloController {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
